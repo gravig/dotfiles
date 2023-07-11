58e68bcfce093e035681e4595f37c5549e6c7d7c
Dawid Grzelecki <davidgrzelecki@gmail.com>
2023-07-11 17:43:48 +0200

    Nvim plugins, i3 screenshot snipping

--- nvim/lua/warsaw/remap.lua
+++ nvim/lua/warsaw/remap.lua
@@ -10,13 +10,45 @@ vim.keymap.set("n", "<C-[>", vim.cmd.BufferPrevious, { desc = "Previous buffer" 
 vim.keymap.set("n", "<C-]>", vim.cmd.BufferNext, { desc = "Next buffer" })
 vim.keymap.set("n", "<C-x>", vim.cmd.BufferClose, { desc = "Close buffer" })
 vim.keymap.set("n", "<A-e>", vim.cmd.EslintFixAll, { desc = "Eslint format" })
+vim.keymap.set("n", "<Esc>", function() end)
 vim.keymap.set("n", "<C-s>", function()
-    if vim.fn.exists(":EslintFixAll") > 0 then 
-       vim.cmd.EslintFixAll()
-    end
+  if vim.fn.exists(":EslintFixAll") > 0 then 
+    vim.cmd.EslintFixAll()
+  end
 
-    if vim.fn.exists(":CocCommand") > 0 then
-    	vim.cmd.CocCommand("prettier.formatFile")	
-    end
-    vim.cmd.w()
+  if vim.fn.exists(":CocCommand") > 0 then
+    vim.cmd.CocCommand("prettier.formatFile")	
+  end
+  vim.cmd.w()
 end, { desc = "Eslint format" })
+
+vim.opt.nu = true
+vim.opt.guicursor = ""
+vim.wo.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.updatetime = 50
+
+vim.opt.scrolloff = 8
+-- vim.opt.colorcolumn = "80"
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<C-d>","<C-d>zz^")
+vim.keymap.set("n", "<C-u>","<C-u>zz^")
--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -120,6 +120,11 @@ _G.packer_plugins = {
     path = "/home/david/.local/share/nvim/site/pack/packer/start/lualine.nvim",
     url = "https://github.com/nvim-lualine/lualine.nvim"
   },
+  ["markdown-preview.nvim"] = {
+    loaded = true,
+    path = "/home/david/.local/share/nvim/site/pack/packer/start/markdown-preview.nvim",
+    url = "https://github.com/iamcco/markdown-preview.nvim"
+  },
   ["mason-lspconfig.nvim"] = {
     loaded = true,
     path = "/home/david/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
@@ -223,14 +228,14 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: which-key.nvim
-time([[Config for which-key.nvim]], true)
-try_loadstring("\27LJ\2\nt\0\0\3\0\a\0\0156\0\0\0009\0\1\0+\1\2\0=\1\2\0006\0\0\0009\0\1\0)\1,\1=\1\3\0006\0\4\0'\2\5\0B\0\2\0029\0\6\0004\2\0\0B\0\2\1K\0\1\0\nsetup\14which-key\frequire\15timeoutlen\ftimeout\6o\bvim\0", "config", "which-key.nvim")
-time([[Config for which-key.nvim]], false)
 -- Config for: nvim-surround
 time([[Config for nvim-surround]], true)
 try_loadstring("\27LJ\2\n?\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\18nvim-surround\frequire\0", "config", "nvim-surround")
 time([[Config for nvim-surround]], false)
+-- Config for: which-key.nvim
+time([[Config for which-key.nvim]], true)
+try_loadstring("\27LJ\2\nt\0\0\3\0\a\0\0156\0\0\0009\0\1\0+\1\2\0=\1\2\0006\0\0\0009\0\1\0)\1,\1=\1\3\0006\0\4\0'\2\5\0B\0\2\0029\0\6\0004\2\0\0B\0\2\1K\0\1\0\nsetup\14which-key\frequire\15timeoutlen\ftimeout\6o\bvim\0", "config", "which-key.nvim")
+time([[Config for which-key.nvim]], false)
 -- Config for: git-conflict.nvim
 time([[Config for git-conflict.nvim]], true)
 try_loadstring("\27LJ\2\n:\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\17git-conflict\frequire\0", "config", "git-conflict.nvim")
